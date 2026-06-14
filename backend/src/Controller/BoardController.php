7298e90b0de9313b8afe8d3f695fc521542ee4a4
AndreiButalov <andrej.butalov@gmx.de>
2026-06-14 13:59:39 +0200

    add BoardController->fixbug

--- backend/src/Controller/BoardController.php
+++ backend/src/Controller/BoardController.php
@@ -2,16 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\BoardRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class BoardController
 {
-    #[Route('/api/boards', name: 'api_boards', methods: ['GET'])]
-    public function index(): JsonResponse
+    #[Route('/api/boards', methods: ['GET'])]
+    public function index(BoardRepository $boardRepository): JsonResponse
     {
-        return new JsonResponse([
-            'message' => 'Board API works!'
-        ]);
+        $boards = $boardRepository->findAll();
+
+        $data = [];
+
+        foreach ($boards as $board) {
+            $data[] = [
+                'id' => $board->getId(),
+                'title' => $board->getTitle(),
+            ];
+        }
+
+        return new JsonResponse($data);
     }
 }
