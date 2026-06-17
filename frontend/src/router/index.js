d1ab9879e238da099aff9b66bd0a1d010380e412
AndreiButalov <andrej.butalov@gmx.de>
2026-06-17 11:46:21 +0200

    clean router/index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,26 +7,10 @@ import TaskView from '../views/TaskView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/boards',
-      name: 'boards',
-      component: BoardView,
-    },
-    {
-      path: '/tasklists',
-      name: 'taskList',
-      component: TaskListView,
-    },
-    {
-      path: '/task/:id',
-      name: 'task',
-      component: TaskView,
-    },
+    { path: '/', name: 'home', component: HomeView, },
+    { path: '/boards', name: 'boards', component: BoardView, },
+    { path: '/tasklists', name: 'taskList', component: TaskListView, },
+    { path: '/task/:id', name: 'task', component: TaskView, },
   ],
 })
 
